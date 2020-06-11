ca23c1aa8a091e6c7fe70b84b69bc417ae35d0db
Shivansh Singhal <shivanshsinghal107@gmail.com>
2020-06-11 19:57:00 +0530

    Update import.py

--- import.py
+++ import.py
@@ -1,4 +1,5 @@
 import os
+import imdb
 
 from flask import Flask, session
 from flask_session import Session
@@ -21,10 +22,12 @@ db.execute('''CREATE TABLE IF NOT EXISTS users (username VARCHAR(16) NOT NULL, p
 db.execute('''CREATE TABLE IF NOT EXISTS email (mail VARCHAR(64) NOT NULL, username VARCHAR(16) NOT NULL,
             FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE,
             PRIMARY KEY(mail, username))''')
-db.execute('''CREATE TABLE IF NOT EXISTS movies (id INTEGER NOT NULL, title VARCHAR(64), release_date text, status VARCHAR(20),
-            rating FLOAT(1), genre VARCHAR(64), cast text, duration INTEGER, summary text, PRIMARY KEY(id))''')
-db.execute('''CREATE TABLE IF NOT EXISTS series (id INTEGER NOT NULL, title VARCHAR(64), release_date text, staus VARCHAR(20),
-            rating FLOAT(1), season INTEGER, episodes INTEGER, cast text, duration INTEGER, genre VARCHAR(64), origin VARCHAR(20), summary text, next_episode text, PRIMARY KEY(id))''')
+#origin, status, person['full-size headshot']
+db.execute('''CREATE TABLE IF NOT EXISTS movies (id INTEGER NOT NULL, title VARCHAR(64), release_date text,
+            rating FLOAT, cast text, genres VARCHAR(100), duration INTEGER, summary text, cover_url text, PRIMARY KEY(id))''')
+#origin, status, person['full-size headshot']
+db.execute('''CREATE TABLE IF NOT EXISTS series (id INTEGER NOT NULL, title VARCHAR(64), release_date text,
+            rating FLOAT, cast text, season INTEGER, episodes INTEGER, duration INTEGER, genres VARCHAR(100), summary text, next_episode text, cover_url text, PRIMARY KEY(id))''')
 db.execute('''CREATE TABLE IF NOT EXISTS mwatched (id INTEGER PRIMARY KEY AUTOINCREMENT, wid INTEGER NOT NULL,
             username VARCHAR(16) NOT NULL, FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE,
             FOREIGN KEY(wid) REFERENCES movies(id) ON DELETE CASCADE ON UPDATE CASCADE)''')
@@ -35,11 +38,29 @@ db.commit()
 db.close()
 
 def main():
+    ia = imdb.IMDb()
+    top250 = ia.get_top250_movies()
+    count = 1
+    for t in top250[:50]:
+        id = t.movieID
+        m = ia.get_movie(id)
+        genres = ""
+        for g in m['genres']:
+            genres += g + ", "
+        genres = genres[:-2]
+        cast = ""
+        for c in m['cast'][:5]:
+            cast += c['name'] + f"({c.currentRole}), "
+        cast = cast[:-2]
+        db.execute("INSERT INTO movies (id, title, release_date, rating, cast, genres, duration, summary, cover_url) VALUES (:id, :title, :release_date, :rating, :cast, :genres, :duration, :summary, :cover_url)", {"id": id, "title": m['title'], "release_date": m['original air date'][:11], "rating": m['rating'], "cast": cast, "genres": genres, "duration": m['runtimes'][0], "summary": m['plot outline'], "cover_url": m['full-size cover url']})
+        db.commit()
+        print(f"{count}.{m['title']} inserted")
+        count += 1
     movies = db.execute("SELECT * FROM movies").fetchall()
     series = db.execute("SELECT * FROM series").fetchall()
     if len(movies) > 0:
         for movie in movies:
-            print(f'{movie.title}\t{movie.year}\tRating {movie.rating}')
+            print(f'Title: {movie.title}\tDuration: {movie.duration} minutes\tRating: {movie.rating}')
     else:
         print("No movies in database till now")
 
@@ -47,7 +68,7 @@ def main():
         for s in series:
             print(f'{s.title}\tSeason {s.season}\tRating {s.rating}')
     else:
-        print("No series in database till now")
+        print("\nNo series in database till now")
 
 if __name__ == "__main__":
     main()
