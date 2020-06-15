a3b175a700a8f0b947da8eb5f8cc5f2c12591799
Shivansh Singhal <shivanshsinghal107@gmail.com>
2020-06-15 18:07:33 +0530

    Added cast id to make addition to watch list faster

--- import.py
+++ import.py
@@ -24,10 +24,10 @@ db.execute('''CREATE TABLE IF NOT EXISTS email (mail VARCHAR(64) NOT NULL, usern
             PRIMARY KEY(mail, username))''')
 # origin, status
 db.execute('''CREATE TABLE IF NOT EXISTS movies (id INTEGER NOT NULL, kind VARCHAR(20), title VARCHAR(64), release text,
-            rating FLOAT, cast text, genres VARCHAR(100), duration INTEGER, budget VARCHAR(40), worldwide_gross VARCHAR(40), summary text, cover_url text, PRIMARY KEY(id))''')
+            rating FLOAT, cast text, cast_id text, genres VARCHAR(100), duration INTEGER, budget VARCHAR(40), worldwide_gross VARCHAR(40), summary text, cover_url text, PRIMARY KEY(id))''')
 # origin, status, next_episode
 db.execute('''CREATE TABLE IF NOT EXISTS series (id INTEGER NOT NULL, kind VARCHAR(20), title VARCHAR(64), release text,
-            rating FLOAT, cast text, seasons INTEGER, episodes INTEGER, genres VARCHAR(100), duration INTEGER, summary text, cover_url text, PRIMARY KEY(id))''')
+            rating FLOAT, cast text, cast_id text, seasons INTEGER, episodes INTEGER, genres VARCHAR(100), duration INTEGER, summary text, cover_url text, PRIMARY KEY(id))''')
 db.execute('''CREATE TABLE IF NOT EXISTS watched (id INTEGER PRIMARY KEY AUTOINCREMENT, wid INTEGER NOT NULL, user_rating INTEGER,
             kind VARCHAR(20), date text, username VARCHAR(16) NOT NULL, FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY(wid) REFERENCES movies(id) ON DELETE CASCADE ON UPDATE CASCADE)''')
 db.commit()
@@ -37,7 +37,7 @@ def main():
     ia = imdb.IMDb()
     top250 = ia.get_top250_movies()
     count = 1
-    for t in top250[:10]:
+    for t in top250[:5]:
         id = t.movieID
         m = ia.get_movie(id)
         plot = ""
@@ -57,15 +57,18 @@ def main():
             genres += g + ", "
         genres = genres[:-2]
         cast = ""
+        cast_id = ""
         #cast_url = ""
         for c in m['cast'][:5]:
             cast += c['name'] + f"({c.currentRole}), "
+            cast_id += f"{c.personID}, "
             #p = ia.get_person(c.personID)
             #if 'full-size headshot' in p.keys():
             #    cast_url += p['full-size headshot'] + ", "
         cast = cast[:-2]
+        cast_id = cast_id[:-2]
         #cast_url = cast_url[:-2]
-        db.execute("INSERT INTO movies (id, kind, title, release, rating, cast, genres, duration, budget, worldwide_gross, summary, cover_url) VALUES (:id, :kind, :title, :release, :rating, :cast, :genres, :duration, :budget, :worldwide_gross, :summary, :cover_url)", {"id": id, "kind": m['kind'], "title": m['title'], "release": m['original air date'][:11], "rating": m['rating'], "cast": cast, "genres": genres, "duration": duration, "budget": budget, "worldwide_gross": gross, "summary": plot, "cover_url": m['full-size cover url']})
+        db.execute("INSERT INTO movies (id, kind, title, release, rating, cast, cast_id, genres, duration, budget, worldwide_gross, summary, cover_url) VALUES (:id, :kind, :title, :release, :rating, :cast, :cast_id, :genres, :duration, :budget, :worldwide_gross, :summary, :cover_url)", {"id": id, "kind": m['kind'], "title": m['title'], "release": m['original air date'][:11], "rating": m['rating'], "cast": cast, "cast_id": cast_id, "genres": genres, "duration": duration, "budget": budget, "worldwide_gross": gross, "summary": plot, "cover_url": m['full-size cover url']})
         db.commit()
         print(f"{count}. {m['title']} inserted")
         count += 1
